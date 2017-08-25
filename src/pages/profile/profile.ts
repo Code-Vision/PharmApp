a7bdb9fa7450ecd46a5ab757c3453709cc285410
Romeo Folie <romeofolie1@gmail.com>
2017-08-25 15:55:32 +0000

    added www directory

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SigninPage } from '../signin/signin';
+// import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the ProfilePage page.
@@ -20,8 +21,10 @@ export class ProfilePage {
   }
 
   tosignin(){
-    // this.navCtrl.pop
-    this.navCtrl.push(SigninPage)
+    // this.navCtrl.pop(TabsPage)
+    // this.navCtrl.push(SigninPage)
+    // this.navCtrl.setRoot(SigninPage);
+    this.navCtrl.popToRoot();
   }
 
   ionViewDidLoad() {
